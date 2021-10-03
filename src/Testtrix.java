ae0f9df714e61d511630d685896f7a0c7f48cd26
BrickGoat <Brickgoat@gmail.com>
2021-10-03 17:02:07 -0400

    main() with currently implemented methods

---
+++ src/Testtrix.java
@@ -0,0 +1,10 @@
+public class Testtrix {
+    public static void main(String[] args) {
+        double[][] valsA = { { 1., 2., 3 }, { 4., 5., 6. }, { 7., 8., 9. } };
+        double[][] valsB = { { 1. }, { 1. }, { 1. } };
+        Matrix A = new Matrix(valsA);
+        Matrix x = new Matrix(valsB);
+        Matrix b = A.times(x);
+
+    }
+}
