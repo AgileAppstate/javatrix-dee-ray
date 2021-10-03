313666495569796fd114d59dab90a52b2f0730e8
BrickGoat <Brickgoat@gmail.com>
2021-10-03 16:35:49 -0400

    added Matrix multiplication method

--- src/Matrix.java
+++ src/Matrix.java
@@ -15,4 +15,20 @@ public class Matrix {
     public double[][] getMatrix() {
         return this.A;
     }
+
+    public Matrix times(Matrix B) {
+        double[][] bArr = B.getMatrix();
+        if (A[0].length != bArr.length) {
+            throw new IllegalArgumentException("Matrix inner dimensions must agree.");
+        }
+        double[][] res = new double[A.length][bArr[0].length];
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < bArr[0].length; j++) {
+                for (int k = 0; k < bArr.length; k++) {
+                    res[i][j] += A[i][k] * bArr[k][j];
+                }
+            }
+        }
+        return new Matrix(res);
+    }
 }
